7a3307be5ed7117f34ea3525bb9df4e3a0e4dc90
Nicolas Camacho <ncamacho1@cuc.edu.co>
2020-07-19 16:32:14 -0500

    :white_check_mark: tests finished but incomplete

--- src/components/Form/Form.test.js
+++ src/components/Form/Form.test.js
@@ -2,18 +2,33 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import Form from './Form';
-import { SetupBox, FormWrapper, CustomInput, Error, FetchButton, CustomLabel } from './ui';
+import { SetupBox, CustomInput, FetchButton, CustomLabel } from './ui';
 
 const clickFn = jest.fn();
 
+const configMock = {
+  url: '',
+  limit: 0,
+  interval: 0
+}
+
 describe('Form', () => {
+
+  const shallowWrapper = shallow(<Form config={configMock} setConfig={clickFn} />);
+
+  it('renders title', () => {
+    expect(shallowWrapper.find('h1').html()).toContain('Setup');
+  });
+
   it('Renders three <CustomInput /> components', () => {
-    const configMock = {
-      url: '',
-      limit: 0,
-      interval: 0
-    }
-    const wrapper = shallow(<Form config={configMock} setConfig={clickFn} />);
-    expect(wrapper.find(CustomInput)).toHaveLength(3);
+    expect(shallowWrapper.find(CustomInput)).toHaveLength(3);
+  })
+
+  it('Renders three <CustomLabel /> components', () => {
+    expect(shallowWrapper.find(CustomLabel)).toHaveLength(3);
+  })
+
+  it('Renders three <CustomLabel /> components', () => {
+    expect(shallowWrapper.find(CustomLabel)).toHaveLength(3);
   })
 });
